79130a5fea84452e00f35a296e3296136f3b8387
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-29 20:53:17 +0300

    Edit Movie Resource Class

--- src/main/java/org/acme/MovieResource.java
+++ src/main/java/org/acme/MovieResource.java
@@ -3,12 +3,14 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/movies")
 public class MovieResource {
@@ -21,10 +23,10 @@ public class MovieResource {
     }
 
     @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/size")
-    public Integer countMovies() {
-        return movies.size();
+    public Response countMovies() {
+        return Response.ok(movies.size()).build();
     }
 
     @POST
